8a51aa32d4de6b18d1f6a1b2ccf97906621224b8
CryZe <cryze92@gmail.com>
2014-08-17 13:58:02 +0200

    Implemented IDisposable

--- UI/Components/TextComponent.cs
+++ UI/Components/TextComponent.cs
@@ -166,5 +166,9 @@ namespace LiveSplit.UI.Components
                 invalidator.Invalidate(0, 0, width, height);
             }
         }
+
+        public void Dispose()
+        {
+        }
     }
 }
