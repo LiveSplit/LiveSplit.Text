5b81877f17b80a1bd5c2a2c5764eb555d89d17a0
Ero <imjustero@gmail.com>
2024-09-02 10:07:10 +0200

    Use file-scoped namespaces, move `using` directives, fix whitespace
    * `IDE0005`, `IDE0065`, `IDE0161`

--- src/LiveSplit.Text/UI/Components/TextComponentFactory.cs
+++ src/LiveSplit.Text/UI/Components/TextComponentFactory.cs
@@ -1,27 +1,27 @@
-﻿using LiveSplit.Model;
+﻿using System;
+
+using LiveSplit.Model;
 using LiveSplit.UI.Components;
-using System;
 
 [assembly: ComponentFactory(typeof(TextComponentFactory))]
 
-namespace LiveSplit.UI.Components
+namespace LiveSplit.UI.Components;
+
+public class TextComponentFactory : IComponentFactory
 {
-    public class TextComponentFactory : IComponentFactory
-    {
-        public string ComponentName => "Text";
+    public string ComponentName => "Text";
 
-        public string Description => "Displays any text that you want it to show.";
+    public string Description => "Displays any text that you want it to show.";
 
-        public ComponentCategory Category => ComponentCategory.Information;
+    public ComponentCategory Category => ComponentCategory.Information;
 
-        public IComponent Create(LiveSplitState state) => new TextComponent(state);
+    public IComponent Create(LiveSplitState state) => new TextComponent(state);
 
-        public string UpdateName => ComponentName;
+    public string UpdateName => ComponentName;
 
-        public string XMLURL => "http://livesplit.org/update/Components/update.LiveSplit.Text.xml";
+    public string XMLURL => "http://livesplit.org/update/Components/update.LiveSplit.Text.xml";
 
-        public string UpdateURL => "http://livesplit.org/update/";
+    public string UpdateURL => "http://livesplit.org/update/";
 
-        public Version Version => Version.Parse("1.8.29");
-    }
+    public Version Version => Version.Parse("1.8.29");
 }
--- src/LiveSplit.Text/UI/Components/TextTextComponent.cs
+++ src/LiveSplit.Text/UI/Components/TextTextComponent.cs
@@ -1,20 +1,19 @@
-﻿namespace LiveSplit.UI.Components
+﻿namespace LiveSplit.UI.Components;
+
+public class TextTextComponent : InfoTextComponent
 {
-    public class TextTextComponent : InfoTextComponent
+    public TextComponentSettings Settings { get; set; }
+
+    public TextTextComponent(TextComponentSettings settings)
+        : base("", "")
     {
-        public TextComponentSettings Settings { get; set; }
+        Settings = settings;
+    }
 
-        public TextTextComponent(TextComponentSettings settings)
-            : base("", "")
-        {
-            Settings = settings;
-        }
-
-        public override void PrepareDraw(Model.LiveSplitState state, LayoutMode mode)
-        {
-            NameMeasureLabel.Font = Settings.OverrideFont1 ? Settings.Font1 : state.LayoutSettings.TextFont;
-            ValueLabel.Font = Settings.OverrideFont2 ? Settings.Font2 : state.LayoutSettings.TextFont;
-            NameLabel.Font = Settings.OverrideFont1 ? Settings.Font1 : state.LayoutSettings.TextFont;
-        }
+    public override void PrepareDraw(Model.LiveSplitState state, LayoutMode mode)
+    {
+        NameMeasureLabel.Font = Settings.OverrideFont1 ? Settings.Font1 : state.LayoutSettings.TextFont;
+        ValueLabel.Font = Settings.OverrideFont2 ? Settings.Font2 : state.LayoutSettings.TextFont;
+        NameLabel.Font = Settings.OverrideFont1 ? Settings.Font1 : state.LayoutSettings.TextFont;
     }
 }
